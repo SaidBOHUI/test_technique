0833a55f7d317492721d7147bfd8a16b239cbf90
Said BOHUI <bohuisaid@hotmail.fr>
2022-01-24 03:25:53 +0100

    projet fonctionel

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require("cors")
 require('dotenv/config')
 
 const app = express();
+app.use(cors());
+
 const uri = process.env.DB_CONNEXION;
+
+// Lancement du serveur
+const PORT = process.env.PORT || 6000
 
 //Parse les données reçues dans le body en json
 app.use(bodyParser.json());
@@ -17,15 +23,15 @@ app.use('/client', clientsRoute);
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('WE ARE HEEERRE');
+    res.send('Bing Bing');
 })
 
-// Lancement du serveur une fois la connection à la base de données est établie
+// Lancement du serveur une fois la connection à la base de données établie
 mongoose.connect(uri, {useNewUrlParser: true})
     .then(() => {
-      app.listen(3000);
-      console.log('conntected')
+      app.listen(process.env.PORT);
+      console.log(`server on port ${PORT}`)
     })
     .catch(err => {
-        console.log("Failed to connect to  mongoose can't start server")
+        console.log("Failed to connect to mongoose can't start server")
     });
